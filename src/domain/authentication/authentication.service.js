62105f273aa5a3a292eccb6bae42a6283c3bc0e2
thibault <thibault.andreis@gmail.com>
2020-12-18 09:35:36 +0100

    proxy error

--- src/domain/authentication/authentication.service.js
+++ src/domain/authentication/authentication.service.js
@@ -39,7 +39,15 @@ export function login(data) {
 }
 
 export function logout() {
-    // TODO   
+  return fetch(`/api/me`)
+    .then(async res => {
+      if (res.status !== 200 && res.status !== 201) {
+        const { message } = await res.json()
+        throw new Error(message)
+      }
+      return res
+    })
+    .then(res => res.json());
 }
 
 export function getMe() {
